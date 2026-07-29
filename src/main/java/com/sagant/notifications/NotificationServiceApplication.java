1366de285c6a12284780185487e5e6939bf9fc33
Stefan <stefan.wrzosek.perez@gmail.com>
2026-07-29 19:45:34 -0300

    fix: agregar clase principal de la aplicacion (faltaba en el commit inicial)

---
+++ src/main/java/com/sagant/notifications/NotificationServiceApplication.java
@@ -0,0 +1,19 @@
+package com.sagant.notifications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+
+@SpringBootApplication
+@EnableAsync
+@EnableRetry
+@EnableScheduling
+public class NotificationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationServiceApplication.class, args);
+    }
+}
